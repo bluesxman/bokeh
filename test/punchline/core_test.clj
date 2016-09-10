0667890d7b4e3948db52b676031f6e572a31a8c3
Jon Newton <jon.newton@gmail.com>
2016-09-10 13:36:42 -0600

    namespace cleanup

--- test/punchline/core_test.clj
+++ test/punchline/core_test.clj
@@ -1,6 +1,6 @@
-(ns bokeh.core-test
+(ns punchline.core-test
   (:require [clojure.test :refer :all]
-            [bokeh.core :refer :all]))
+            [punchline.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
